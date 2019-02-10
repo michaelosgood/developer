bd69499ddfac706e41bbdf21fe2291701fd3984f
Michael Osgood <michael.f.osgood@gmail.com>
2019-02-09 17:12:22 -0800

    updated navbar

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,10 +3,19 @@ let prevScrollpos = window.pageYOffset;
 window.onscroll = function() {
     let currentScrollpos = window.pageYOffset;
         if (prevScrollpos > currentScrollpos) {
-            document.getElementById('navbar').style.top = '0';
+            document.getElementsByClassName('navbar').style.top = '0';
         } 
         else {
-            document.getElementById('navbar').style.top = '-100px';
+            document.getElementsByClassName('navbar').style.top = '-100px';
         }
   prevScrollpos = currentScrollpos;
 }
+
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "navbar") {
+      x.className += " responsive";
+    } else {
+      x.className = "navbar";
+    }
+  }
