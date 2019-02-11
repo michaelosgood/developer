d231ef1f3acfd09ae6a72462adf5d6c26150fc74
Michael Osgood <michael.f.osgood@gmail.com>
2019-02-11 15:26:39 -0800

    reverted to original navbar

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,19 +3,10 @@ let prevScrollpos = window.pageYOffset;
 window.onscroll = function() {
     let currentScrollpos = window.pageYOffset;
         if (prevScrollpos > currentScrollpos) {
-            document.getElementById('myTopnav').style.top = '0';
+            document.getElementById('navbar').style.top = '0';
         } 
         else {
-            document.getElementById('myTopnav').style.top = '-50px';
+            document.getElementById('navbar').style.top = '-100px';
         }
   prevScrollpos = currentScrollpos;
 }
-
-function myFunction() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "navbar") {
-      x.className += " responsive";
-    } else {
-      x.className = "navbar";
-    }
-  }
